6b36f7296f6283075a4fc8faff4fa9349ba96449
Rohit kumar <78663043+rohit-kumar-kushwaha@users.noreply.github.com>
2022-01-29 14:10:05 +0530

    send otp

--- smartcontactmanager/src/main/java/com/smart/controller/ForgotPasswordController.java
+++ smartcontactmanager/src/main/java/com/smart/controller/ForgotPasswordController.java
@@ -2,36 +2,63 @@ package com.smart.controller;
 
 import java.util.Random;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.smart.service.EmailService;
+
 @Controller
 public class ForgotPasswordController {
-	
+
 	Random random = new Random(100000);
-	
-	//email id form open handler
+
+	@Autowired
+	private EmailService emailService;
+
+	// email id form open handler
 	@RequestMapping("/forgot")
-	public String openEmailForm() {
+	public String openEmailForm(Model model) {
 		
+		model.addAttribute("title", "Forgot password");
 		return "forgot_email_form";
 	}
-	
-	//email id form open handler
-		@PostMapping("/send-otp")
-		public String sendOTP(@RequestParam("email") String email) {
+
+	// send otp handler
+	@PostMapping("/send-otp")
+	public String sendOTP(@RequestParam("email") String email, HttpSession session, Model model) {
 			
-			System.out.println("EMAIL : "+email);
+		System.out.println("EMAIL : "+email);
+		
+		model.addAttribute("title", "OTP");
 			
-//			generating otp of 4 digits
-			
-			
-			int otp = random.nextInt(999999);
-			System.out.println("OTP :"+otp);
+//		generating otp of 4 digits
 			
 			
+		int otp = random.nextInt(999999);
+		System.out.println("OTP :"+otp);
+		String subject = "OPT From Smart Contact Manager";
+		String message = "<h1> OTP = " +otp+ "</h1>";
+		String to = email;
+			
+		boolean flag = this.emailService.sendEmail(subject, message, to);
+		
+		if(flag) {
+			
+			session.setAttribute("otp", otp);
 			return "verify_otp";
 		}
+		else {
+			
+			session.setAttribute("message", "Check your email id !!");
+			return "forgot_email_form";
+		}
+			
+		
+	}
 }
---
+++ smartcontactmanager/src/main/java/com/smart/service/EmailService.java
@@ -0,0 +1,83 @@
+package com.smart.service;
+
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+	public boolean sendEmail(String subject, String message, String to) {
+
+		boolean flag = false;
+		String host = "smtp.gmail.com";
+
+//		get the system property
+		Properties properties = System.getProperties();
+
+		String from = "rohit.pumca@gmail.com";
+
+//		setting important information to properties object
+
+//		host set
+		properties.put("mail.smtp.host", host);
+		properties.put("mail.smtp.port", "465");
+		properties.put("mail.smtp.ssl.enable", "true");
+		properties.put("mail.smtp.auth", "true");
+
+//		get the session object
+		Session session = Session.getInstance(properties, new Authenticator() {
+
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+
+				return new PasswordAuthentication("rohit.pumca@gmail.com", "@3888Rohit");
+			}
+
+		});
+
+		session.setDebug(true);
+
+//		step 2 : compose the mail
+		MimeMessage mimeMessage = new MimeMessage(session);
+
+		try {
+
+//			from email
+			mimeMessage.setFrom(from);
+
+//			adding recipient to message
+			mimeMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+//			adding subject to message
+			mimeMessage.setSubject(subject);
+
+//			adding text to message
+			mimeMessage.setText(message);
+
+//			step 3 : send
+//			send the message using Transport class
+			Transport.send(mimeMessage);
+
+			System.out.println("send successfully.......");
+
+			flag = true;
+
+		} catch (MessagingException e) {
+
+			e.printStackTrace();
+		}
+		return flag;
+
+	}
+
+}
